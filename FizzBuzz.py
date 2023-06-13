f0044bfb1a0b70225bcbea72288f6d12b3aff98e
Yegeta Taye <96721353+yt61304325@users.noreply.github.com>
2023-06-14 00:59:46 +0300

    Update FizzBuzz.py

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -1,1 +1,24 @@
+from typing import List
 
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+
+        result = []
+
+        for index in range(1, n+1):
+
+            if ((index % 3 == 0) and (index % 5 == 0)):
+                result.append("FizzBuzz")
+            elif index % 3 == 0:
+                result.append("Fizz")
+            elif index % 5 == 0:
+                result.append("Buzz")
+            else:
+                result.append(str(index))
+
+        return result
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.fizzBuzz(15))
